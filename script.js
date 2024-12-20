898d39ee5f055c3eb7721a57ae391f9c427b7da3
Sworaj07 <sworajkhandual1998@gmail.com>
2024-12-20 13:02:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function OfFun(){
+ let img=document.getElementById("image")
+ img.src="on.jpg.png"
+}
+function OnFun(){
+    let img=document.getElementById("image")
+    img.src="ofbulb.jpg"
+}
